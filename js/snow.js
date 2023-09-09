3a60610d64179ebd2cbdcac4fd2c59a3a3bd5c67
Santos <91850681+Santyxs@users.noreply.github.com>
2023-09-10 01:48:49 +0200

    Update snow.js

--- js/snow.js
+++ js/snow.js
@@ -1,40 +1,72 @@
-particlesJS("SnowFall",
-    {
-        "particles": {
-            "number": {
-                "value": 80
-            },
-            "shape": {
-                "type": "circle",
-            },
-            "size": {
-                "value": 7,
-                "random": true
-            },
-            "line_linked": {
-                "enable": false
-            },
-            "move": {
-                "enable": true,
-                "speed": 5,
-                "direction": "bottom",
-                "straight": false
-            }
-        },
-        "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-                "onhover": {
-                    "enable": false
-                }
-            },
-            "modes": {
-                "push": {
-                    "particles_nb": 12
-                }
-            }
+const doParticles = true;
+
+const getWidth = () => {
+    return Math.max(
+        document.body.scrollWidth,
+        document.documentElement.scrollWidth,
+        document.body.offsetWidth,
+        document.documentElement.offsetWidth,
+        document.documentElement.clientWidth
+    );
+};
+
+const handleCopyIP = () => {
+    const copy = document.createElement("textarea");
+    copy.style.position = "absolute";
+    copy.style.left = "-99999px";
+    copy.style.top = "0";
+    copy.setAttribute("id", "ta");
+    document.body.appendChild(copy);
+    copy.textContent = t;
+    copy.select();
+    document.execCommand("copy");
+    $(".ip").html("<span class='extrapad'>IP copiada!</span>");
+    setTimeout(() => {
+        $(".ip").html(t);
+        const copy = document.getElementById("ta");
+        copy.parentNode.removeChild(copy);
+    }, 800);
+};
+
+const updatePlayercount = (ip, port) => {
+    $.get(`https://api.bybilly.uk/api/players/${ip}/${port}`, (result) => {
+        if (result.hasOwnProperty('online')) {
+            $(".sip").html(result.online);
+        } else {
+            $(".playercount").html("El servidor no está disponible!");
         }
+    });
+};
+
+if (doParticles) {
+    const particleSettings = getWidth() < 400 ? {
+        minPixel: 1,
+        maxPixel: 2,
+        total: 20
+    } : {
+        minPixel: 1,
+        maxPixel: 3,
+        total: 40
+    };
+    $.firefly(particleSettings);
+}
+
+let t;
+$(document).ready(() => {
+    t = $(".ip").html();
+});
+
+$(document).on("click", ".ip", handleCopyIP);
+
+$(document).ready(() => {
+    const ip = $(".sip").attr("data-ip");
+    let port = $(".sip").attr("data-port") || "25565";
+    if (!ip) {
+        console.error("Error al obtener el recuento de jugadores");
+    } else {
+        updatePlayercount(ip, port);
+        setInterval(() => {
+            updatePlayercount(ip, port);
+        }, 60000);
     }
-
-
-);
+});
